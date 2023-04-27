d676c026f9d287478cb67807b87f7e56de36154a
hossam <hossamyehya477@gmail.com>
2023-04-27 12:56:55 +0200

    Update Three

--- app.js
+++ app.js
@@ -14,7 +14,22 @@ const stripeRoute = require("./routes/stripe")
 const Comments = require("./routes/comment")
 const complaints = require("./routes/complaint")
 // Cors
-app.use(cors())
+app.use(function(req, res, next) {
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
+
+app.options("*", cors());
+app.use(cors());
 /////////////// 
 dotenv.config()
 app.use(express.json())
@@ -36,19 +51,6 @@ app.get("*", (req, res) => {
   res.send("Hello")
 })
 
-app.use(function(req, res, next) {
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  next();
-});
 
 const port = process.env.PORT || 5000
 app.listen(port, () => console.log("Server Working"))
